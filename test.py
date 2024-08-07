d6e5448a6a245957fda4dfa71a7268bab1ae81c2
albe <albedehombre@icloud.com>
2024-08-07 10:32:11 -0500

    created CSV dump

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('fart')
+import csv
+def dumpCSV(file='/home/albede/Desktop/Practicum Projects/Practicum-Sprint-1/Sanford+Stuff+Catalogue+-+Sheet1.csv'):
+    with open(file, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        names, descriptions, quantities = [],[],[]
+        
+        for row in spamreader:
+            info = ' '.join(row).split(',')
+            quantities.insert(-1, info[-1])
+            names.insert(-1, info[0])
+
+            des = ''
+            for i in range(1, len(info) - 1): des += info[i]
+            descriptions.insert(-1, des)
+
+        return names, descriptions, quantities
+    return
+
+
+names, descriptions, quantities = dumpCSV()
+print(names[3])
